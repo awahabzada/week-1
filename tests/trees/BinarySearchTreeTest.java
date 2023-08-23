f2ef5c38268e4c1c66631cd8859908b784bef948
John Roberts <roberts.john@gmail.com>
2023-08-22 20:43:22 -0700

    Wrote our first test

---
+++ tests/trees/BinarySearchTreeTest.java
@@ -0,0 +1,10 @@
+package tests.trees;
+
+import org.junit.jupiter.api.Test;
+
+public class BinarySearchTreeTest {
+  @Test
+  public void testExistence() {
+    BinarySearchTree tree = new BinarySearchTree();
+  }
+}
